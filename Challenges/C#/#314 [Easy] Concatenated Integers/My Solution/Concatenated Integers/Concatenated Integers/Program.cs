f917b0a2874fb51f69a44f39627f93417c02f29e
Prashant-Naicker <prashantnaicker@gmail.com>
2017-06-30 22:29:55 +1200

    Finished Challenge #314

--- Challenges/C#/#314 [Easy] Concatenated Integers/My Solution/Concatenated Integers/Concatenated Integers/Program.cs
+++ Challenges/C#/#314 [Easy] Concatenated Integers/My Solution/Concatenated Integers/Concatenated Integers/Program.cs
@@ -12,24 +12,71 @@ namespace Concatenated_Integers
 
         static void Main(string[] args)
         {
-            string maxDig = "0";
+            string lowestNum = "";
+            string largestNum = "";       
+            string suitNum = "";
+
+            int Count = 0;       
 
             Console.WriteLine("Enter a list of integers:");
             string line = Console.ReadLine();
 
-            list = addLineToList(line);   
+            list = addLineToList(line);
 
-            for (int i = 0; i < list.Count(); i++)
+            string[] finalList = new string[list.Count];
+
+            while (list.Count > 0) 
             {
-                if (maxDig[0] == list[i][0])
-                {
-                    if (maxDig.Length == 1) { continue; }
-                    if (list[i].Length == 1) { maxDig = list[i]; continue; }
+                suitNum = list[list.Count - 1];
+                for (int j = 0; j < list.Count; j++)
+                {                  
+                    suitNum = findSuitableNumber(suitNum, list[j]);
                 }
-                if (maxDig[0] < list[i][0]) {
-                    maxDig = list[i];
-                }         
+                finalList[Count] = suitNum;
+                list.Remove(suitNum);
+                Count++;
             }
+            
+            for (int i = 0; i < finalList.Length; i++)
+            {
+                largestNum += finalList[i];
+            }
+
+            for (int i = finalList.Length - 1; i >= 0; i--)
+            {
+                lowestNum += finalList[i];
+            }
+
+            Console.WriteLine(largestNum);
+            Console.WriteLine(lowestNum);
+            Console.ReadLine();
+
+        }
+
+        static string findSuitableNumber(string num1, string num2)
+        {
+            string suitableNum = num1;
+            int sizeNum1 = num1.Length;
+            int sizeNum2 = num2.Length;
+            int sizeDiff = Math.Abs(sizeNum2 - sizeNum1);
+
+            int smallerSize = sizeNum1 > sizeNum2 ? sizeNum2 : sizeNum1;
+            bool num1Bigger = sizeNum1 > sizeNum2 ? true : false;
+
+            for (int i = 0; i < smallerSize; i++)
+            {
+                if (num1[i] == num2[i]) { continue; }
+                suitableNum = num1[i] > num2[i] ? num1 : num2;
+                return suitableNum;
+            }
+
+            for (int i = smallerSize; i < smallerSize + sizeDiff; i++)
+            {
+                if (num1Bigger) { suitableNum = num1[i] > num2[0] ? num1 : num2; }
+                else { suitableNum = num2[i] > num1[0] ? num2 : num1;  }
+            }
+
+            return suitableNum;
         }
 
         static List<string> addLineToList(string line)
@@ -45,6 +92,8 @@ namespace Concatenated_Integers
                     continue;
                 }
                 currentVal += line[i];
+
+                if (i == line.Length - 1) { list.Add(currentVal); }
             }
             return list;
         }
