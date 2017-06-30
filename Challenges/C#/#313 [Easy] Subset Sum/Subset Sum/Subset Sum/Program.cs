d047573303f3a5c46ba2232b073b93bbe0c90ad7
Prashant-Naicker <prashantnaicker@gmail.com>
2017-07-01 11:46:30 +1200

    Added Challenge #313 Solution

---
+++ Challenges/C#/#313 [Easy] Subset Sum/Subset Sum/Subset Sum/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+delegate List<int> sortList(int[] a);
+
+namespace Subset_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a set of numbers:");
+            string[] strVals = Console.ReadLine().Split(' ');
+
+            int[] intVals = Array.ConvertAll(strVals, s => int.Parse(s));
+
+            sortList n = getAllNegatives;
+            sortList p = getAllPositives;
+
+            List<int> negatives = n(intVals);
+            List<int> positives = p(intVals);
+
+            int negativesTotal = getTotal(negatives);
+            int positivesTotal = getTotal(positives);
+
+            if (negativesTotal + positivesTotal == 0)
+            {
+                Console.WriteLine("true");
+                Console.ReadLine();
+            }
+            Console.WriteLine("false");
+            Console.ReadLine();
+        }
+
+        static List<int> getAllNegatives(int[] arr)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] < 0) { list.Add(arr[i]);  }
+            }
+
+            return list;
+        }
+
+        static List<int> getAllPositives(int[] arr)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0) { list.Add(arr[i]); }
+            }
+
+            return list;
+        }
+
+        static int getTotal(List<int> list)
+        {
+            int total = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                total += list[i];
+            }
+
+            return total;
+        }
+    }
+}
